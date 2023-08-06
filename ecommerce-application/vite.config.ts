2120ac738d56c6557957535cefc1aa313486e296
unknown <besssta888@mail.ru>
2023-08-06 22:20:16 +0300

    feat: install and configure Vitest, create and add Vitest test script

--- ecommerce-application/vite.config.ts
+++ ecommerce-application/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
@@ -5,4 +6,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [react()],
   base: './',
+  test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['text'],
+    },
+    environment: 'jsdom',
+  },
 });
