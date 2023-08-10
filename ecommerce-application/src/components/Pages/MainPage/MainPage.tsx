e0b51ff1cc831ad512cbf5d03b66459d8d8090ba
antasth <antasth@mail.ru>
2023-08-10 17:06:15 +0300

    feat: add Header in MainPage component

--- ecommerce-application/src/components/Pages/MainPage/MainPage.tsx
+++ ecommerce-application/src/components/Pages/MainPage/MainPage.tsx
@@ -1,8 +1,10 @@
+import { Header } from '../../common/Header/Header';
 import { MainPageMain } from './MainPageMain/MainPageMain';
 
 export function MainPage(): React.ReactElement {
   return (
     <div>
+      <Header />
       <MainPageMain />
     </div>
   );
