e32f70446d28d4d2c4da6f1f2952432e7dd73d0e
Alyona <besssta888@mail.ru>
2023-08-10 11:00:53 +0300

    feat: add registration page to project

---
+++ ecommerce-application/src/components/Pages/RegistrationPage/RegistrationPage.tsx
@@ -0,0 +1,11 @@
+import RegistrationPageMain from './RegistrationPageMain/RegistrationPageMain';
+
+function RegistrationPage(): React.ReactElement {
+  return (
+    <div>
+      <RegistrationPageMain />
+    </div>
+  );
+}
+
+export default RegistrationPage;
---
+++ ecommerce-application/src/components/Pages/RegistrationPage/RegistrationPageMain/RegistrationPageMain.tsx
@@ -0,0 +1,9 @@
+function RegistrationPageMain(): React.ReactElement {
+  return (
+    <main>
+      <h1>Registration page content</h1>
+    </main>
+  );
+}
+
+export default RegistrationPageMain;
