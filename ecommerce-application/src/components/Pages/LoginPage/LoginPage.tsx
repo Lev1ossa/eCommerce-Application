77abecef195ac951baf8e9d74934d850b0a6334e
Alyona <besssta888@mail.ru>
2023-08-10 10:46:28 +0300

    feat: add login page to project

---
+++ ecommerce-application/src/components/Pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,11 @@
+import LoginPageMain from './LoginPageMain/LoginPageMain';
+
+function LoginPage(): React.ReactElement {
+  return (
+    <main>
+      <LoginPageMain />
+    </main>
+  );
+}
+
+export default LoginPage;
---
+++ ecommerce-application/src/components/Pages/LoginPage/LoginPageMain/LoginPageMain.tsx
@@ -0,0 +1,9 @@
+function LoginPageMain(): React.ReactElement {
+  return (
+    <div>
+      <h1>Login page content</h1>
+    </div>
+  );
+}
+
+export default LoginPageMain;
