ef899884b22147b7b0962ddea3baf40def64b23f
antasth <antasth@mail.ru>
2023-08-10 09:06:57 +0300

    feat: add MainPage component

---
+++ ecommerce-application/src/components/Pages/MainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import { MainPageMain } from './MainPageMain/MainPageMain';
+
+function MainPage(): React.ReactElement {
+  return (
+    <main>
+      <MainPageMain />
+    </main>
+  );
+}
+
+export { MainPage };
