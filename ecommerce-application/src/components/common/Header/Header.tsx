03353562910a4dd553aebbb9f11e1080e2717119
antasth <antasth@mail.ru>
2023-08-10 16:38:47 +0300

    feat: add link to h1 in Header component

--- ecommerce-application/src/components/common/Header/Header.tsx
+++ ecommerce-application/src/components/common/Header/Header.tsx
@@ -3,7 +3,9 @@ import styles from './Header.module.css';
 export function Header(): React.ReactElement {
   return (
     <header className={styles.header}>
-      <h1>Ecommerce-application</h1>
+      <a href="/#">
+        <h1>Ecommerce-application</h1>
+      </a>
       <nav>
         <ul>
           <li>
