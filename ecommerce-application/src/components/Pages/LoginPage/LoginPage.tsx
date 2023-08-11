b58513ad0064a9e0f1fbbb960e33dcaf9d530205
Alyona <besssta888@mail.ru>
2023-08-11 16:38:47 +0300

    feat: add styles for LoginPage

--- ecommerce-application/src/components/Pages/LoginPage/LoginPage.tsx
+++ ecommerce-application/src/components/Pages/LoginPage/LoginPage.tsx
@@ -1,9 +1,10 @@
 import { Header } from '../../common/Header/Header';
-import LoginPageMain from './LoginPageMain/LoginPageMain';
+import { LoginPageMain } from './LoginPageMain/LoginPageMain';
+import styles from './LoginPage.module.css';
 
 export function LoginPage(): React.ReactElement {
   return (
-    <div>
+    <div className={styles.loginPage__container}>
       <Header />
       <LoginPageMain />
     </div>
--- ecommerce-application/src/components/Pages/LoginPage/LoginPageMain/LoginPageMain.tsx
+++ ecommerce-application/src/components/Pages/LoginPage/LoginPageMain/LoginPageMain.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
+import styles from './LoginPageMain.module.css';
 
+// eslint-disable-next-line max-lines-per-function
 export function LoginPageMain(): React.ReactElement {
   const [data, setData] = useState({ username: '', password: '' });
   function handleFormSubmit(event: React.FormEvent): void {
@@ -12,29 +14,35 @@ export function LoginPageMain(): React.ReactElement {
     }
   }
   return (
-    <main>
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <label htmlFor="usernameInput">
-          Username
-          <input
-            id="usernameInput"
-            type="text"
-            value={data.username}
-            onChange={(e): void => handleInputChange(e, 'username')}
-          />
-        </label>
-        <label htmlFor="passwordInput">
-          Password
-          <input
-            id="passwordInput"
-            type="password"
-            value={data.password}
-            onChange={(e): void => handleInputChange(e, 'password')}
-          />
-        </label>
-        <button type="submit">Log in</button>
-      </form>
+    <main className={styles.loginPage__main}>
+      <div className={styles.container}>
+        <h2>Login</h2>
+        <form onSubmit={handleFormSubmit}>
+          <label htmlFor="usernameInput">
+            Username
+            <input
+              id="usernameInput"
+              type="text"
+              value={data.username}
+              onChange={(e): void => handleInputChange(e, 'username')}
+            />
+            <div />
+          </label>
+          <label htmlFor="passwordInput">
+            Password
+            <input
+              id="passwordInput"
+              type="password"
+              value={data.password}
+              onChange={(e): void => handleInputChange(e, 'password')}
+            />
+            <div />
+          </label>
+          <button type="submit">Log in</button>
+        </form>
+        <p>Don&apos;t have an account yet?</p>
+        <button type="button">Sign up now</button>
+      </div>
     </main>
   );
 }
