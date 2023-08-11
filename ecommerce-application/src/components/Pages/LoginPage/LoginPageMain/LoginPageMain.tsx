a8e9fd57ecd62f458637737f943e05e8af3bfdf6
Alyona <besssta888@mail.ru>
2023-08-11 15:12:59 +0300

    feat: add form with input elements in LoginPageMain

--- ecommerce-application/src/components/Pages/LoginPage/LoginPageMain/LoginPageMain.tsx
+++ ecommerce-application/src/components/Pages/LoginPage/LoginPageMain/LoginPageMain.tsx
@@ -1,9 +1,40 @@
-function LoginPageMain(): React.ReactElement {
+import { useState } from 'react';
+
+export function LoginPageMain(): React.ReactElement {
+  const [data, setData] = useState({ username: '', password: '' });
+  function handleFormSubmit(event: React.FormEvent): void {
+    event.preventDefault();
+  }
+  function handleInputChange(event: React.ChangeEvent, text: string): void {
+    const { target } = event;
+    if (target instanceof HTMLInputElement) {
+      setData({ ...data, [text]: target.value });
+    }
+  }
   return (
     <main>
-      <h3>Login page content</h3>
+      <h2>Login</h2>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="usernameInput">
+          Username
+          <input
+            id="usernameInput"
+            type="text"
+            value={data.username}
+            onChange={(e): void => handleInputChange(e, 'username')}
+          />
+        </label>
+        <label htmlFor="passwordInput">
+          Password
+          <input
+            id="passwordInput"
+            type="password"
+            value={data.password}
+            onChange={(e): void => handleInputChange(e, 'password')}
+          />
+        </label>
+        <button type="submit">Log in</button>
+      </form>
     </main>
   );
 }
-
-export default LoginPageMain;
