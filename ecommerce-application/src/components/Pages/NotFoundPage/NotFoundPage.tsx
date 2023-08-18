a41e248e09475477638d22b1b300308df26a282e
antasth <antasth@mail.ru>
2023-08-18 23:35:12 +0300

    feat: add basic structure and styles for notFound page

--- ecommerce-application/src/components/Pages/NotFoundPage/NotFoundPage.tsx
+++ ecommerce-application/src/components/Pages/NotFoundPage/NotFoundPage.tsx
@@ -1,11 +1,5 @@
-import { Header } from '../../common/Header/Header';
 import { NotFoundPageMain } from './NotFoundPageMain/NotFoundPageMain';
 
 export function NotFoundPage(): React.ReactElement {
-  return (
-    <div>
-      <Header />
-      <NotFoundPageMain />
-    </div>
-  );
+  return <NotFoundPageMain />;
 }
--- ecommerce-application/src/components/Pages/NotFoundPage/NotFoundPageMain/NotFoundPageMain.tsx
+++ ecommerce-application/src/components/Pages/NotFoundPage/NotFoundPageMain/NotFoundPageMain.tsx
@@ -1,7 +1,17 @@
+import styles from './NotFoundPageMain.module.scss';
+
 export function NotFoundPageMain(): React.ReactElement {
   return (
-    <main>
-      <h3>404 PAGE IS NOT FOUND</h3>
+    <main className={styles.container}>
+      <ul className={styles.colors}>
+        <li className={styles.colors_one} />
+        <li className={styles.colors_two} />
+        <li className={styles.colors_three} />
+        <li className={styles.colors_four} />
+      </ul>
+      <div className={styles.content}>
+        <h1>404</h1>
+      </div>
     </main>
   );
 }
