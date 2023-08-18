800207f2c79cc38f01d962ec3c601bf2314941a1
antasth <antasth@mail.ru>
2023-08-19 01:42:12 +0300

    feat: add content for 404 page

--- ecommerce-application/src/components/Pages/NotFoundPage/NotFoundPageMain/NotFoundPageMain.tsx
+++ ecommerce-application/src/components/Pages/NotFoundPage/NotFoundPageMain/NotFoundPageMain.tsx
@@ -1,5 +1,7 @@
+import { Link } from 'react-router-dom';
 import styles from './NotFoundPageMain.module.scss';
 
+// eslint-disable-next-line max-lines-per-function
 export function NotFoundPageMain(): React.ReactElement {
   return (
     <main className={styles.container}>
@@ -9,8 +11,39 @@ export function NotFoundPageMain(): React.ReactElement {
         <li className={styles.colors_three} />
         <li className={styles.colors_four} />
       </ul>
-      <div className={styles.content}>
-        <h1>404</h1>
+      <div className={styles.page_content}>
+        <div className={styles.wrapper}>
+          <div className={styles.content}>
+            <div className={styles.header}>
+              <div className={styles.logo}>Logo</div>
+              <nav className={styles.nav}>
+                <ul className={styles.links}>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/login">Login</Link>
+                  </li>
+                  <li>
+                    <Link to="/registration">Registration</Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div className={styles.main}>
+              <div className={styles.message}>
+                <div className={styles.message_content}>
+                  <div className={styles.error}>404</div>
+                  <div className={styles.title}>Something went wrong!</div>
+                  <button className={styles.button} type="button">
+                    Back to Homepage
+                  </button>
+                </div>
+              </div>
+              <div className={styles.image} />
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
