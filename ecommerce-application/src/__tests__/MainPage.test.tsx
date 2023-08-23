b23110b481ae477d3863a0ad5eb7bb099c03c8cd
antasth <antasth@mail.ru>
2023-08-23 23:09:11 +0300

    refactor: create different files for each test

---
+++ ecommerce-application/src/__tests__/MainPage.test.tsx
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+
+import { BrowserRouter } from 'react-router-dom';
+import { MainPage } from '../components/Pages/MainPage/MainPage';
+
+describe('Renders MainPage correctly', async () => {
+  it('Should render the MainPage correctly', async () => {
+    render(
+      <BrowserRouter>
+        <MainPage />
+      </BrowserRouter>,
+    );
+  });
+});
