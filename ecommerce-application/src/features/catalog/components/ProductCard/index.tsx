14942f4d584efee39c35849fbb1f1dacce7944ec
antasth <antasth@mail.ru>
2023-08-27 20:42:57 +0300

    refactor: remove tasks from another card

--- ecommerce-application/src/features/catalog/components/ProductCard/index.tsx
+++ ecommerce-application/src/features/catalog/components/ProductCard/index.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import { IProduct } from '../../../../types/types';
 import img from '../../assets/orange-1.png';
 import styles from './ProductCard.module.scss';
@@ -9,16 +8,14 @@ export function ProductCard(props: { product: IProduct }): React.ReactElement {
   return (
     <div className={styles.card}>
       <img src={img} className={styles.image} alt="product" />
-      <Link to={`/product/${name}`} state={product}>
-        <div className={styles.description}>
-          <div className={styles.product_info}>
-            <strong className={styles.price}>${price}</strong>
-            <p className={styles.name}>{name}</p>
-            <p className={styles.category}>Category:{category}</p>
-            <p className={styles.tm}>TM:{tm}</p>
-          </div>
+      <div className={styles.description}>
+        <div className={styles.product_info}>
+          <strong className={styles.price}>${price}</strong>
+          <p className={styles.name}>{name}</p>
+          <p className={styles.category}>Category:{category}</p>
+          <p className={styles.tm}>TM:{tm}</p>
         </div>
-      </Link>
+      </div>
       <button type="button" className={styles.button}>
         Add to cart
       </button>
