df33a52dc9d6925df5fcf76ad51644b73d953029
antasth <antasth@mail.ru>
2023-08-27 18:04:42 +0300

    feat: add Product page to app router

--- ecommerce-application/src/App/routes/router.tsx
+++ ecommerce-application/src/App/routes/router.tsx
@@ -4,6 +4,7 @@ import { MainPage } from '../../Pages/MainPage/MainPage';
 import { NotFoundPage } from '../../Pages/NotFoundPage/NotFoundPage';
 import { RegistrationPage } from '../../Pages/RegistrationPage/RegistrationPage';
 import { CatalogPage } from '../../Pages/CatalogPage/CatalogPage';
+import { ProductPage } from '../../Pages/ProductPage/ProductPage';
 
 export const router = createBrowserRouter([
   {
@@ -14,6 +15,7 @@ export const router = createBrowserRouter([
       { path: 'login', element: <LoginPage /> },
       { path: 'registration', element: <RegistrationPage /> },
       { path: 'catalog', element: <CatalogPage /> },
+      { path: 'product', element: <ProductPage /> },
     ],
   },
 ]);
