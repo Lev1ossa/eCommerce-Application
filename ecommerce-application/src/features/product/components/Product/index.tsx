49143db5bcedf21648e468394f75f98a60770c82
antasth <antasth@mail.ru>
2023-08-27 18:44:57 +0300

    feat: add Product component basic structure

--- ecommerce-application/src/features/product/components/Product/index.tsx
+++ ecommerce-application/src/features/product/components/Product/index.tsx
@@ -3,7 +3,18 @@ import styles from './Product.module.scss';
 export function Product(): React.ReactElement {
   return (
     <div className={styles.product}>
-      <h1>Product</h1>
+      <div className={styles.container}>
+        <div className={styles.slider}>SLIDER</div>
+        <div className={styles.details}>
+          <div className={styles.name}>Lorem ipsum dolor sit.</div>
+          <div className={styles.price}>$ 1.55</div>
+          <div className={styles.category}>Citrus</div>
+          <div className={styles.trademark}>Victoria</div>
+          <button type="button" className={styles.button}>
+            Add to cart
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
