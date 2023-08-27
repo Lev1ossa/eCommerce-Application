c6f14845bc3fd0e293b35de4925cedd27e502078
antasth <antasth@mail.ru>
2023-08-27 10:01:58 +0300

    refactor: change reexport in features index files

--- ecommerce-application/src/features/autentification/index.tsx
+++
@@ -1,5 +0,0 @@
-import { LoginForm } from './components/LoginForm';
-import { RegistrationForm } from './components/RegistrationForm';
-import { handleLogout } from './utils/authHandlers';
-
-export { LoginForm, RegistrationForm, handleLogout };
