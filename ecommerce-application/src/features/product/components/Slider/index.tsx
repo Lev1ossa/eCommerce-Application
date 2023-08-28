4d523b868030f0b9527e98f8bc93e83dc7313ca3
antasth <antasth@mail.ru>
2023-08-28 20:24:11 +0300

    feat: add styles for product slider

--- ecommerce-application/src/features/product/components/Slider/index.tsx
+++ ecommerce-application/src/features/product/components/Slider/index.tsx
@@ -7,38 +7,35 @@ import styles from './Slider.module.scss';
 
 export function Slider(): React.ReactElement {
   return (
-    <>
-      {/* <div className={styles.slider}> */}
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={30}
-        loop
-        pagination={{
-          clickable: true,
-        }}
-        navigation
-        modules={[Pagination, Navigation]}
-        className={styles.swiper}
-      >
-        <SwiperSlide className={styles.swiper_slide}>
-          <img
-            src="https://thumbs.dreamstime.com/b/green-apple-leaf-slice-isolated-white-ripe-background-clipping-path-45471482.jpg"
-            alt="test"
-          />
-        </SwiperSlide>
-        <SwiperSlide className={styles.swiper_slide}>
-          <img
-            src="https://thumbs.dreamstime.com/b/red-apple-isolated-clipping-path-19130134.jpg"
-            alt="test"
-          />
-        </SwiperSlide>
-        <SwiperSlide className={styles.swiper_slide}>
-          <img
-            src="https://thumbs.dreamstime.com/b/red-apple-leaf-slice-white-background-29914331.jpg"
-            alt="test"
-          />
-        </SwiperSlide>
-      </Swiper>
-    </>
+    <Swiper
+      slidesPerView={1}
+      spaceBetween={30}
+      loop
+      pagination={{
+        clickable: true,
+      }}
+      navigation
+      modules={[Pagination, Navigation]}
+      className={styles.swiper}
+    >
+      <SwiperSlide className={styles.swiper_slide}>
+        <img
+          src="https://thumbs.dreamstime.com/b/green-apple-leaf-slice-isolated-white-ripe-background-clipping-path-45471482.jpg"
+          alt="test"
+        />
+      </SwiperSlide>
+      <SwiperSlide className={styles.swiper_slide}>
+        <img
+          src="https://thumbs.dreamstime.com/b/red-apple-isolated-clipping-path-19130134.jpg"
+          alt="test"
+        />
+      </SwiperSlide>
+      <SwiperSlide className={styles.swiper_slide}>
+        <img
+          src="https://thumbs.dreamstime.com/b/red-apple-leaf-slice-white-background-29914331.jpg"
+          alt="test"
+        />
+      </SwiperSlide>
+    </Swiper>
   );
 }
