7139511436a281077f861e51e60b9628e2a3ba6d
antasth <antasth@mail.ru>
2023-08-29 11:53:09 +0300

    feat: add basic styles and logic for modal

--- ecommerce-application/src/features/modal/components/Modal/index.tsx
+++ ecommerce-application/src/features/modal/components/Modal/index.tsx
@@ -1,5 +1,24 @@
+import { Dispatch, SetStateAction } from 'react';
 import styles from './Modal.module.scss';
 
-export function Modal(): React.ReactElement {
-  return <div className={styles.modal}>modal</div>;
+export function Modal(props: {
+  active: boolean;
+  setActive: Dispatch<SetStateAction<boolean>>;
+}): React.ReactElement {
+  const { active, setActive } = props;
+  return (
+    <div
+      className={active ? `${styles.modal} ${styles.active}` : styles.modal}
+      onClick={(): void => setActive(false)}
+      aria-hidden
+    >
+      <div
+        className={styles.content}
+        onClick={(e): void => e.stopPropagation()}
+        aria-hidden
+      >
+        modal
+      </div>
+    </div>
+  );
 }
