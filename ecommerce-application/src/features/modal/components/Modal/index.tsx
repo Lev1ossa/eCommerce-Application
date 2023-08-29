269d51345049c5428c912c7319ed2a89cf85f43c
antasth <antasth@mail.ru>
2023-08-29 12:12:15 +0300

    feat: add modal content appearance animation

--- ecommerce-application/src/features/modal/components/Modal/index.tsx
+++ ecommerce-application/src/features/modal/components/Modal/index.tsx
@@ -8,12 +8,16 @@ export function Modal(props: {
   const { active, setActive } = props;
   return (
     <div
-      className={active ? `${styles.modal} ${styles.active}` : styles.modal}
+      className={
+        active ? `${styles.modal} ${styles.modal_active}` : styles.modal
+      }
       onClick={(): void => setActive(false)}
       aria-hidden
     >
       <div
-        className={styles.content}
+        className={
+          active ? `${styles.content} ${styles.content_active}` : styles.content
+        }
         onClick={(e): void => e.stopPropagation()}
         aria-hidden
       >
