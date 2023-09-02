ec1e2b777a85cbcaaaec18091484d4a625f80a21
Alyona <besssta888@mail.ru>
2023-09-03 01:10:52 +0300

    feat: add creating address

--- ecommerce-application/src/features/profile/components/Addresses/Addresses.tsx
+++ ecommerce-application/src/features/profile/components/Addresses/Addresses.tsx
@@ -1,10 +1,12 @@
-import { LiaShippingFastSolid } from 'react-icons/lia';
 import { useState } from 'react';
+import { AiOutlineHome } from 'react-icons/ai';
+import { BiEditAlt } from 'react-icons/bi';
 import styles from './Addresses.module.scss';
 import { AddressCard } from '../AddressCard/AddressCard';
 import { Modal } from '../../../modal';
 import { UserAdress } from '../../../../types/types';
 import { AddressCardActive } from '../AddressCardActive/AddressCardActive';
+import { NewAddressCard } from '../NewAddressCard/NewAddressCard';
 
 const addressesData = [
   {
@@ -56,14 +58,27 @@ const addressesData = [
 // eslint-disable-next-line max-lines-per-function
 export function Addresses(): React.ReactElement {
   const [modalActive, setModalActive] = useState(false);
+  const [modalCreateAddressActive, setModalCreateAddressActive] =
+    useState(false);
   const [modalAddressId, setModalAddressId] = useState('0xe7kPdI');
   const data = addressesData.find((el) => el.id === modalAddressId);
+  const handleCreateAddressButton = (): void => {
+    setModalCreateAddressActive(true);
+  };
   return (
     <>
       <div className={styles.article}>
         <h3 className={styles.title}>
-          <LiaShippingFastSolid className={styles.icon} />
-          ADDRESSES
+          <AiOutlineHome className={styles.icon} />
+          Addresses
+          <button
+            className={styles.edit_button}
+            onClick={handleCreateAddressButton}
+            type="button"
+          >
+            <BiEditAlt className={styles.edit_button_icon} />
+            Add Address
+          </button>
         </h3>
         <div className={styles.addresses_container}>
           {addressesData.map((addressData) => {
@@ -96,6 +111,26 @@ export function Addresses(): React.ReactElement {
               styles={styles}
               addressData={data as UserAdress}
             />
+          </div>
+        </Modal>
+      )}
+      {modalCreateAddressActive && (
+        <Modal
+          active={modalCreateAddressActive}
+          setActive={setModalCreateAddressActive}
+        >
+          <div className={styles.modal}>
+            <header className={styles.modal_header}>
+              <h3>Create address</h3>
+              <button
+                type="button"
+                className={styles.modal_button}
+                onClick={(): void => setModalCreateAddressActive(false)}
+              >
+                X
+              </button>
+            </header>
+            <NewAddressCard styles={styles} />
           </div>
         </Modal>
       )}
