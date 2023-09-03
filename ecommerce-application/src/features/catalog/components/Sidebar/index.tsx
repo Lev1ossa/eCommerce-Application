ec4fa18b20897d51e688031173465d8599a35e6b
antasth <antasth@mail.ru>
2023-09-03 22:56:05 +0300

    feat: add brands and origin lists

--- ecommerce-application/src/features/catalog/components/Sidebar/index.tsx
+++ ecommerce-application/src/features/catalog/components/Sidebar/index.tsx
@@ -4,6 +4,7 @@ import {
   MenuItem,
   Sidebar,
   SubMenu,
+  menuClasses,
   sidebarClasses,
 } from 'react-pro-sidebar';
 import styles from './Sidebar.module.scss';
@@ -22,7 +23,12 @@ export function CatalogSidebar(): React.ReactElement {
       >
         <Menu
           rootStyles={{
-            position: 'sticky',
+            [`.${menuClasses.button}`]: {
+              ':hover': {
+                backgroundColor: '#64e44c',
+              },
+            },
+            // position: 'sticky',
             top: '1.5rem',
           }}
         >
@@ -50,26 +56,72 @@ export function CatalogSidebar(): React.ReactElement {
           <SubMenu label="Filters" defaultOpen>
             <ul className={styles.list}>
               <li className={styles.price}>
-                <span>Price</span>
+                <strong>Price</strong>
                 <input type="text" placeholder="min" />
                 <input type="text" placeholder="max" />
+              </li>
+              <li className={styles.brand}>
+                <ul className={styles.brand_list}>
+                  <strong>Brands</strong>
+                  <li>
+                    <input type="checkbox" />
+                    <span className="text">Barbados</span>
+                  </li>
+                  <li>
+                    <input type="checkbox" />
+                    <span className="text">Campari</span>
+                  </li>
+                  <li>
+                    <input type="checkbox" />
+                    <span className="text">Cara Navel</span>
+                  </li>
+                  <li>
+                    <input type="checkbox" />
+                    <span className="text">Cosmic Crisp</span>
+                  </li>
+                  <li>
+                    <input type="checkbox" />
+                    <span className="text">Dream</span>
+                  </li>
+                  <li>
+                    <input type="checkbox" />
+                    <span className="text">Victoria</span>
+                  </li>
+                  <li>
+                    <input type="checkbox" />
+                    <span className="text">Vegan</span>
+                  </li>
+                </ul>
+              </li>
+              <li className={styles.brand}>
+                <ul className={styles.brand_list}>
+                  <strong>Origin</strong>
+                  <li>
+                    <input type="checkbox" />
+                    <span className="text">Local</span>
+                  </li>
+                  <li>
+                    <input type="checkbox" />
+                    <span className="text">Foreign</span>
+                  </li>
+                </ul>
               </li>
             </ul>
           </SubMenu>
           <SubMenu label="Sort" defaultOpen>
-            <ul className={styles.list}>
+            <ul className={`${styles.list} ${styles.brand_list}`}>
               <li>
-                <input className="checkbox" type="checkbox" />
-                <span className="text">Name</span>
+                <input type="checkbox" />
+                <span>Name</span>
               </li>
               <li>
-                <input className="checkbox" type="checkbox" /> Price
+                <input type="checkbox" /> Price
               </li>
               <li>
-                <input className="checkbox" type="checkbox" /> Origin
+                <input type="checkbox" /> Origin
               </li>
               <li>
-                <input className="checkbox" type="checkbox" /> TM
+                <input type="checkbox" /> TM
               </li>
             </ul>
           </SubMenu>
