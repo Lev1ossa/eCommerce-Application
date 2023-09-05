4d335a75c9508934f6dd7eb834fbf9304ac48147
Lev1ossa <ikehhui@gmail.com>
2023-09-05 03:25:36 +0300

    refactor: remove commented code

--- ecommerce-application/src/features/breadcrumb/components/Breadcrumps/Breadcrumb.tsx
+++ ecommerce-application/src/features/breadcrumb/components/Breadcrumps/Breadcrumb.tsx
@@ -58,30 +58,3 @@ export function Breadcrumb(props: Record<string, string>): React.ReactElement {
     </div>
   );
 }
-
-// const propsValuesArr = Object.values(props);
-//   const dataArr = propsValuesArr.map((el): string | null => {
-//     if (el) {
-//       const newStr = el[0].toUpperCase() + el.slice(1);
-//       return newStr.replace('_', ' ');
-//     }
-//     return null;
-//   });
-//   const componentsArr = dataArr.map((el, i) => {
-//     if (i === dataArr.length - 1) {
-//       return (
-//         <button className={styles.button} type="button">
-//           {el}
-//         </button>
-//       );
-//     }
-//     return (
-//       <>
-//         <button className={styles.button} type="button">
-//           {el}
-//         </button>
-//         <FiChevronsRight className={styles.icon} />
-//       </>
-//     );
-//   });
-// return <div className={styles.container}>{componentsArr}</div>;
