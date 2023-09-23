521f11529345f8b2fa00c4f79f3853b3da49927c
Alyona <besssta888@mail.ru>
2023-09-24 00:51:33 +0300

    fix: fix title positiion

--- ecommerce-application/src/Pages/MainPage/MainPage.tsx
+++ ecommerce-application/src/Pages/MainPage/MainPage.tsx
@@ -25,27 +25,31 @@ export function MainPage(): React.ReactElement {
       <main className={styles.main}>
         <MainSlider />
         <section className={styles.promocodes_block}>
-          <h3 className={styles.title}>PROMO CODES</h3>
-          <div className={styles.image_block}>
-            <div className={styles.image_lemon} />
-            <div className={styles.text_block}>
-              <h4 className={styles.subtitle}>Citrus</h4>
-              <p className={styles.text}>Get Up to 30% Off Citrus</p>
-              <p className={styles.text_color}>Promo code: citrus_summer</p>
+          <h3 className={styles.title_container}>
+            <span className={styles.title}>PROMO CODES</span>
+          </h3>
+          <div className={styles.images_container}>
+            <div className={styles.image_block}>
+              <div className={styles.image_lemon} />
+              <div className={styles.text_block}>
+                <h4 className={styles.subtitle}>Citrus</h4>
+                <p className={styles.text}>Get Up to 30% Off Citrus</p>
+                <p className={styles.text_color}>Promo code: citrus_summer</p>
+              </div>
+              <div className={styles.discount}>
+                30% <span className={styles.discount_color}>OFF</span>
+              </div>
             </div>
-            <div className={styles.discount}>
-              30% <span className={styles.discount_color}>OFF</span>
-            </div>
-          </div>
-          <div className={styles.image_block}>
-            <div className={styles.image_broccoli} />
-            <div className={styles.text_block}>
-              <h4 className={styles.subtitle}>Broccoli</h4>
-              <p className={styles.text}>Get Up to 25% Off Broccoli</p>
-              <p className={styles.text_color}>Promo code: broccoli_dream</p>
-            </div>
-            <div className={styles.discount}>
-              25% <span className={styles.discount_color}>OFF</span>
+            <div className={styles.image_block}>
+              <div className={styles.image_broccoli} />
+              <div className={styles.text_block}>
+                <h4 className={styles.subtitle}>Broccoli</h4>
+                <p className={styles.text}>Get Up to 25% Off Broccoli</p>
+                <p className={styles.text_color}>Promo code: broccoli_dream</p>
+              </div>
+              <div className={styles.discount}>
+                25% <span className={styles.discount_color}>OFF</span>
+              </div>
             </div>
           </div>
         </section>
