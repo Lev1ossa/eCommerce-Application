b1aa227a2cc2efcd7aa855be2a5a89642e3727bc
Alyona <besssta888@mail.ru>
2023-09-23 23:51:49 +0300

    fix: fix error in Header test

--- ecommerce-application/src/__tests__/Footer.test.tsx
+++ ecommerce-application/src/__tests__/Footer.test.tsx
@@ -3,7 +3,7 @@ import { describe, expect, it } from 'vitest';
 import { Footer } from '../components/Footer/Footer';
 
 describe('Renders footer correctly', async () => {
-  it('Should render footr correctly', async () => {
+  it('Should render footer correctly', async () => {
     render(<Footer />);
     const p = screen.queryByText('2023 Good Food');
     expect(p).not.toBeNull();
